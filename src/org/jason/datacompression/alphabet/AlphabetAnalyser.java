1f76134654a287da9c6d54aca5855f71761a0e35
Jason Tsai <a8832a0066@gmail.com>
2015-10-15 16:02:48 +0800

    add AlphabetAnalyser.java
    Signed-off-by: Jason Tsai <a8832a0066@gmail.com>

---
+++ src/org/jason/datacompression/alphabet/AlphabetAnalyser.java
@@ -0,0 +1,34 @@
+package org.jason.datacompression.alphabet;
+
+public class AlphabetAnalyser {
+	private String oreign;
+	private AlphabetMap map;
+	
+	public AlphabetAnalyser(String oreign,AlphabetMap map){
+		setString(oreign);
+		setAlphabetMap(map);
+	}
+	
+	public void setString(String oreign){
+		this.oreign = oreign;
+	}
+	
+	private void setAlphabetMap(AlphabetMap map){
+		this.map = map;
+	}
+	
+	private boolean isAlphabet(char s){
+		if( ('a' <= s && 'z' >= s) || ('A' <= s && 'Z' >= s))
+			return true;
+		else
+			return false;
+	}
+	
+	public void analyseString(){
+		char[] oreignCharArr = oreign.toCharArray();
+		for(char s : oreignCharArr){
+			if(isAlphabet(s))
+				map.addAlphabetCount(s);
+		}
+	}
+}
