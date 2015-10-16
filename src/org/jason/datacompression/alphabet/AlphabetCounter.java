c452bd3066cb62510123f0e19ca792b73431757f
Jason Tsai <a8832a0066@gmail.com>
2015-10-17 00:52:30 +0800

    edit AlphabetCounter for better UI
    Signed-off-by: Jason Tsai <a8832a0066@gmail.com>

--- src/org/jason/datacompression/alphabet/AlphabetCounter.java
+++ src/org/jason/datacompression/alphabet/AlphabetCounter.java
@@ -6,16 +6,21 @@ public class AlphabetCounter {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		if(args[0] == null){
+		if(args.length < 1){
 			System.out.println("alphabetCounter:");
 			System.out.println("use alphabetCounter <file location>");
 		}else{
 			try{
-				AlphabetUI ui = new AlphabetUI(args[0]);
-				if(ui.startAnalyse())
-					ui.printAnalyseChart();
-				else
-					throw new NullPointerException("ui.startAnalyse is wrong.");
+				for(String s : args){
+					AlphabetUI ui = new AlphabetUI(s);
+					if(ui.startAnalyse()){
+						System.out.println("File location: "+s+"\n");
+						System.out.println(ui.printAnalyseChart());
+						System.out.println("==============================\n");
+					}
+					else
+						throw new NullPointerException("ui.startAnalyse is wrong.");
+				}
 			}catch(NullPointerException e){
 				System.out.println("This application has some wrong.");
 				System.out.println(e.getMessage());
