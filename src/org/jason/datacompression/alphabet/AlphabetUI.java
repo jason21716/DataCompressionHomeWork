346c8ebae6dea3eaab6c804144039660a0a4ee5a
Jason Tsai <a8832a0066@gmail.com>
2015-10-19 10:55:25 +0800

    add Entropy counting in UI.java
    Signed-off-by: Jason Tsai <a8832a0066@gmail.com>

--- src/org/jason/datacompression/alphabet/AlphabetUI.java
+++ src/org/jason/datacompression/alphabet/AlphabetUI.java
@@ -1,6 +1,7 @@
 package org.jason.datacompression.alphabet;
 
 import java.io.*;
+import java.math.*;
 public class AlphabetUI {
 	private BufferedReader txtFileReader;
 	private AlphabetMap map;
@@ -32,14 +33,22 @@ public class AlphabetUI {
 	
 	public String printAnalyseChart(){
 		int totalCount = map.getTotalAlphabet();
+		double entropy = 0.0;
 		String Chart = "";
 		for(char i = 'a',j = 'A'; i <= 'z' || j <= 'Z' ; i++,j++){
 			double lowerCasepercent = (double)map.getAlphabetCount(i)/totalCount;
 			double upperCasepercent = (double)map.getAlphabetCount(j)/totalCount;
+			if(lowerCasepercent > 0)
+				entropy += lowerCasepercent * ( Math.log(lowerCasepercent)/Math.log(2) );
+			if(upperCasepercent > 0)
+				entropy += upperCasepercent * ( Math.log(upperCasepercent)/Math.log(2) );
 			Chart += String.format("%c:\t%.7f(%d/%d)\t\t", i,lowerCasepercent,map.getAlphabetCount(i),totalCount);
 			Chart += String.format("%c:\t%.7f(%d/%d)\n", j,upperCasepercent,map.getAlphabetCount(j),totalCount);
 		}
+		entropy *= -1;
+		
 		Chart += String.format("\ntotal:\t1.0000000(%d/%d)\n",totalCount,totalCount);
+		Chart += String.format("\nEntropy:%.7f bits\n",entropy);
 		return Chart;
 	}
 }
